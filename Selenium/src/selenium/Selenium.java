cbf9626c43cc32e0cd0af8564a568e686837ea75
Neuber <neubertavares@gmail.com>
2021-11-08 22:40:38 -0300

    Add projeto Selenium

---
+++ Selenium/src/selenium/Selenium.java
@@ -0,0 +1,22 @@
+
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+
+public class Selenium {
+
+    public static void main(String[] args) {
+        abrirChrome();
+    }
+    private static void abrirChrome() {
+        File file = new File("C:\\Users\\eMotion\\Desktop\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
+        driver.get("hTp:aluno.unibh.br");
+        String i = driver.getCurrentUrl();
+        System.out.println(i);
+        driver.close();
+    }
+}
