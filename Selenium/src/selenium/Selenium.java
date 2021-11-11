ef16ccc090f1c070acfeb2e5ae3d2a3700c70da6
neubertavares <79878757+neubertavares@users.noreply.github.com>
2021-11-10 23:31:19 -0300

    Update Selenium.java
    edit cod.

--- Selenium/src/selenium/Selenium.java
+++ Selenium/src/selenium/Selenium.java
@@ -4,19 +4,28 @@ package selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class Selenium {
+public class drive {
 
-    public static void main(String[] args) {
-        abrirChrome();
-    }
-    private static void abrirChrome() {
+   public static void main(String[] args) {
+
         File file = new File("C:\\Users\\eMotion\\Desktop\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         WebDriver driver = new ChromeDriver();
-        driver.get("http:aluno.unibh.br");
-        String i = driver.getCurrentUrl();
-        System.out.println(i);
-        driver.close();
+        driver.get("https://www.casasbahia.com.br");
+
+        //driver.close();
+        WebElement caixaDeBusca = driver.findElement(By.id("strBusca"));
+        caixaDeBusca.sendKeys("vocês são os melhores");
+        caixaDeBusca.clear();
+        caixaDeBusca.sendKeys("aspirador de po robo");
+
+        WebElement botaoBusca = driver.findElement(By.id("btnOK"));
+        botaoBusca.click();
+
+      //driver.close();
     }
+
 }
